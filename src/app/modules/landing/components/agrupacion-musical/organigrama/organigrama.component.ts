17b306388b2a729cb5e6580984f22feb408cefe7
aarongmez18 <aarongmez18@gmail.com>
2025-09-20 18:10:52 +0200

    Creación del Home y modificaciones

--- src/app/modules/landing/components/agrupacion-musical/organigrama/organigrama.component.ts
+++ src/app/modules/landing/components/agrupacion-musical/organigrama/organigrama.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-organigrama',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./organigrama.component.scss']
 })
 export class OrganigramaComponent {
+
+        constructor(private router: Router) {}
+    
+      navigateTo(url: string) {
+        const [path, fragment] = url.split('#');
+    
+        this.router.navigate([path], { fragment: fragment }).then(() => {
+         
+          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        });
+      }
+  
 
 }
